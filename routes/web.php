25e62f5ae1100ba8cab7cd52f9f39086bb8b658c
asdf <asdf@asdf.com>
2019-09-04 15:19:24 +0900

    엘로퀀드 ORM

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,29 @@
 |
 */
 
-/*
-Route::get('/', function () {
-    $items = ['apple','banana','tomato'];
+Route::get('/', 'WelcomeController@index');
 
-    return view('welcome',['items'=>$items]);
+Route::get('auth/login', function() {
+    $credentials = [
+        'email' => 'aa@aa.com',
+        'password' => 'password',
+    ];
+
+    if(! auth()->attempt($credentials)) {
+        return '로그인 정보가 일치하지 않습니다.';
+    }
+
+    return redirect('protected');
 });
 
-*/
+Route::get('protected', ['middleware' => 'auth', function(){
 
-//Route::get('/', 'WelcomeController@index');
+}]);
 
-Route::resource('articles','ArticlesController');
+Route::get('auth/logout', function(){
+    auth()->logout();
+    return '또 봐요';
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
